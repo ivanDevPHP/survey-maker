f9e2ff6ec1d2ee9881640c2a512b3cc8ad7796d6
Ivan <ivanfslima@gmail.com>
2025-01-21 20:07:33 +0000

    Survey CRUD

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('/survey', [\App\Http\Controllers\SurveyController::class]);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
