3210e74bc9d88ddf413fb2c38db8b36679d4dc24
Ivan <ivanfslima@gmail.com>
2025-01-22 00:41:08 +0000

    Add Image to Survey

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -7,6 +7,9 @@ use App\Models\Survey;
 use App\Http\Requests\StoreSurveyRequest;
 use App\Http\Requests\UpdateSurveyRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
 class SurveyController extends Controller
 {
     /**
@@ -23,9 +26,15 @@ class SurveyController extends Controller
      */
     public function store(StoreSurveyRequest $request)
     {
-        $result = Survey::create($request->validated());
+        $data = $request->validated();
 
-        return new SurveyResource($result);
+        if(isset($data['image'])){
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+        $survey = Survey::create($data);
+
+        return new SurveyResource($survey);
 
 
     }
@@ -48,7 +57,20 @@ class SurveyController extends Controller
      */
     public function update(UpdateSurveyRequest $request, Survey $survey)
     {
-        $survey->update($request->validated());
+        $data = $request->validated();
+
+        if(isset($data['image'])){
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+
+            // If there is an old image, delete it
+            if($survey->image){
+                $absolutePath= public_path($survey->image);
+                File::delete($absolutePath);
+            }
+        }
+
+        $survey->update($data);
         return new SurveyResource($survey);
     }
 
@@ -64,5 +86,41 @@ class SurveyController extends Controller
 
         $survey->delete();
         return response('', 204);
+    }
+
+    private function saveImage($image){
+
+        //Check if image is valid base64
+        if(preg_match('/^data:image\/(\w+);base64,/', $image, $type)){
+            //Take out the base64 encoded text
+            $image = substr($image, strpos($image, ',') + 1);
+
+            //Get file extension
+            $type = strtolower($type[1]);
+
+            //Check if file is on image
+            if(!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])){
+                throw new \Exception('Invalid image type.');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if($image === false){
+                throw new \Exception('Failed to base64 image.');
+            }
+        }else{
+            throw new \Exception('Did not match data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+        if(!File::exists($absolutePath)){
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
     }
 }
