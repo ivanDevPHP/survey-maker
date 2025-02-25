49bf3a1b1e7734ea542380bd55cc902d0a04af04
Ivan <ivanfslima@gmail.com>
2025-02-25 21:25:25 +0000

    refactor: move dashboard logic to SurveyService

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -1,63 +1,86 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreSurveyAnswerRequest;
-use App\Http\Resources\SurveyResource;
-use App\Models\Survey;
 use App\Http\Requests\StoreSurveyRequest;
 use App\Http\Requests\UpdateSurveyRequest;
-use App\Models\SurveyAnswer;
-use App\Models\SurveyQuestion;
-use App\Models\SurveyQuestionAnswer;
+use App\Services\SurveyService;
+use App\Http\Resources\SurveyResource;
+use App\Models\Survey;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
-use Illuminate\Validation\Rule;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class SurveyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request)
+    protected $surveyService;
+
+    public function __construct(SurveyService $surveyService)
     {
-        $user = $request->user();
-        return SurveyResource::collection(Survey::where('user_id', $user->id)->paginate(5));
+        $this->surveyService = $surveyService;
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return AnonymousResourceCollection
      */
-    public function store(StoreSurveyRequest $request)
+    public function index(Request $request): AnonymousResourceCollection
     {
-        $data = $request->validated();
+        return $this->surveyService->index($request);
+    }
 
-        if(isset($data['image'])){
-            $relativePath = $this->saveImage($data['image']);
-            $data['image'] = $relativePath;
-        }
-        $survey = Survey::create($data);
-
-        foreach ($data['questions'] as $question) {
-            $question['survey_id'] = $survey->id;
-            $this->createQuestion($question);
-        }
-
+    /**
+     * @param StoreSurveyRequest $request
+     * @return SurveyResource
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(StoreSurveyRequest $request): SurveyResource
+    {
+        $survey = $this->surveyService->store($request);
         return new SurveyResource($survey);
-
-
     }
 
     /**
-     * Display the specified resource.
+     * @param StoreSurveyAnswerRequest $request
+     * @param Survey $survey
+     * @return Response
      */
-    public function show(Survey $survey, Request $request)
+    public function storeAnswer(StoreSurveyAnswerRequest $request, Survey $survey): Response
+    {
+        return $this->surveyService->storeAnswer($survey, $request);
+    }
+
+    /**
+     * @param UpdateSurveyRequest $request
+     * @param Survey $survey
+     * @return SurveyResource
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(UpdateSurveyRequest $request, Survey $survey): SurveyResource
+    {
+        $updatedSurvey = $this->surveyService->update($survey, $request);
+        return new SurveyResource($updatedSurvey);
+    }
+
+    /**
+     * @param Survey $survey
+     * @param Request $request
+     * @return Response
+     */
+    public function destroy(Survey $survey, Request $request): Response
+    {
+        return $this->surveyService->destroy($survey);
+    }
+
+    /**
+     * @param Survey $survey
+     * @param Request $request
+     * @return SurveyResource
+     */
+    public function show(Survey $survey, Request $request): SurveyResource
     {
         $user = $request->user();
-        if($user->id !== $survey->user_id){
+        if ($user->id !== $survey->user_id) {
             return abort(403, 'Unauthorized action.');
         }
 
@@ -65,187 +88,11 @@ class SurveyController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * @param Survey $survey
+     * @return SurveyResource
      */
-    public function showForGuest(Survey $survey)
+    public function showForGuest(Survey $survey): SurveyResource
     {
         return new SurveyResource($survey);
-    }
-
-    public function storeAnswer(StoreSurveyAnswerRequest $request, Survey $survey)
-    {
-        $validated = $request->validated();
-
-        $surveyAnswer = SurveyAnswer::create([
-           'survey_id' => $survey->id,
-           'start_date' => date('Y-m-d H:i:s'),
-           'end_date'  => date('Y-m-d H:i:s'),
-        ]);
-
-        foreach($validated['answers'] as $questionId => $answer){
-            $question = SurveyQuestion::where(['id' => $questionId, 'survey_id' => $survey->id])->get();
-            if(!$question){
-                return response("Invalid question ID: \"$questionId\"", 400);
-            }
-
-            $data = [
-                'survey_question_id' => $questionId,
-                'survey_answer_id' => $surveyAnswer->id,
-                'answer' => is_array($answer) ? json_encode($answer) : $answer
-            ];
-
-            SurveyQuestionAnswer::create($data);
-        }
-
-        return response("", 201);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateSurveyRequest $request, Survey $survey)
-    {
-        $data = $request->validated();
-
-        if(isset($data['image'])){
-            $relativePath = $this->saveImage($data['image']);
-            $data['image'] = $relativePath;
-
-            // If there is an old image, delete it
-            if($survey->image){
-                $absolutePath= public_path($survey->image);
-                File::delete($absolutePath);
-            }
-        }
-        $survey->update($data);
-
-        $existingIds = $survey->questions()->pluck('id')->toArray();
-
-        $newIds  = Arr::pluck($data['questions'], 'id');
-
-        $toDelete = array_diff($existingIds, $newIds);
-
-        $toAdd = array_diff($newIds, $existingIds);
-
-        SurveyQuestion::destroy($toDelete);
-
-        foreach($data['questions'] as $question){
-            if(in_array($question['id'], $toAdd)){
-                $question['survey_id'] = $survey->id;
-                $this->createQuestion($question);
-            }
-        }
-
-        //Update existing questions
-        $questionMap = collect($data['questions'])->keyBy('id');
-        foreach ($survey->questions as $question){
-            if(isset($questionMap[$question->id])){
-                $this->updateQuestion($question,  $questionMap[$question->id]);
-            }
-        }
-
-        return new SurveyResource($survey);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Survey $survey, Request $request)
-    {
-        $user = $request->user();
-        if($user->id !== $survey->user_id){
-            return abort(403, 'Unauthorized action.');
-        }
-
-        $survey->delete();
-
-        // If there is an old image, delete it
-        if($survey->image){
-            $absolutePath= public_path($survey->image);
-            File::delete($absolutePath);
-        }
-
-        return response('', 204);
-    }
-
-    private function saveImage($image){
-
-        //Check if image is valid base64
-        if(preg_match('/^data:image\/(\w+);base64,/', $image, $type)){
-            //Take out the base64 encoded text
-            $image = substr($image, strpos($image, ',') + 1);
-
-            //Get file extension
-            $type = strtolower($type[1]);
-
-            //Check if file is on image
-            if(!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])){
-                throw new \Exception('Invalid image type.');
-            }
-            $image = str_replace(' ', '+', $image);
-            $image = base64_decode($image);
-
-            if($image === false){
-                throw new \Exception('Failed to base64 image.');
-            }
-        }else{
-            throw new \Exception('Did not match data URI with image data');
-        }
-
-        $dir = 'images/';
-        $file = Str::random() . '.' . $type;
-        $absolutePath = public_path($dir);
-        $relativePath = $dir . $file;
-        if(!File::exists($absolutePath)){
-            File::makeDirectory($absolutePath, 0755, true);
-        }
-        file_put_contents($relativePath, $image);
-
-        return $relativePath;
-    }
-
-    private function createQuestion($data)
-    {
-        if (is_array($data['data'])){
-            $data['data'] = json_encode($data['data']);
-        }
-
-        $validator = Validator::make($data, [
-            'question' => 'required|string',
-            'type' => ['required', Rule::in ([
-                Survey::TYPE_RADIO,
-                Survey::TYPE_CHECKBOX,
-                Survey::TYPE_SELECT,
-                Survey::TYPE_TEXT,
-                Survey::TYPE_TEXTAREA
-            ])],
-            'description' => 'nullable|string',
-            'data' => 'present',
-            'survey_id' => 'exists:App\Models\Survey,id'
-        ]);
-
-        return SurveyQuestion::create($validator->validated());
-    }
-
-    private function updateQuestion(SurveyQuestion $question, $data){
-        if(is_array($data['data'])){
-            $data['data'] = json_encode($data['data']);
-        }
-
-        $validator = Validator::make($data, [
-            'question' => 'required|string',
-            'type' => ['required', Rule::in ([
-                Survey::TYPE_RADIO,
-                Survey::TYPE_CHECKBOX,
-                Survey::TYPE_SELECT,
-                Survey::TYPE_TEXT,
-                Survey::TYPE_TEXTAREA
-            ])],
-            'description' => 'nullable|string',
-            'data' => 'present',
-            'survey_id' => 'exists:App\Models\Survey,id'
-        ]);
-
-        return $question->update($validator->validated());
     }
 }
---
+++ app/Services/SurveyService.php
@@ -0,0 +1,260 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Resources\SurveyResource;
+use App\Models\Survey;
+use App\Models\SurveyAnswer;
+use App\Models\SurveyQuestion;
+use App\Models\SurveyQuestionAnswer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\StoreSurveyRequest;
+use App\Http\Requests\UpdateSurveyRequest;
+use App\Http\Requests\StoreSurveyAnswerRequest;
+
+class SurveyService
+{
+    /**
+     * @param StoreSurveyRequest $request
+     * @return Survey
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(StoreSurveyRequest $request): Survey
+    {
+        $data = $request->validated();
+
+        if (isset($data['image'])) {
+            $data['image'] = $this->saveImage($data['image']);
+        }
+
+        $survey = Survey::create($data);
+
+        foreach ($data['questions'] as $question) {
+            $question['survey_id'] = $survey->id;
+            $this->createQuestion($question);
+        }
+
+        return $survey;
+    }
+
+    /**
+     * @param Survey $survey
+     * @param StoreSurveyAnswerRequest $request
+     * @return Response
+     */
+    public function storeAnswer(Survey $survey, StoreSurveyAnswerRequest $request): Response
+    {
+        $validated = $request->validated();
+
+        $surveyAnswer = SurveyAnswer::create([
+            'survey_id' => $survey->id,
+            'start_date' => now(),
+            'end_date' => now(),
+        ]);
+
+        foreach ($validated['answers'] as $questionId => $answer) {
+            $question = SurveyQuestion::where(['id' => $questionId, 'survey_id' => $survey->id])->first();
+
+            if (!$question) {
+                return response("Invalid question ID: \"$questionId\"", 400);
+            }
+
+            $data = [
+                'survey_question_id' => $questionId,
+                'survey_answer_id' => $surveyAnswer->id,
+                'answer' => is_array($answer) ? json_encode($answer) : $answer
+            ];
+
+            SurveyQuestionAnswer::create($data);
+        }
+
+        return response("", 201);
+    }
+
+    /**
+     * @param Survey $survey
+     * @param UpdateSurveyRequest $request
+     * @return Survey
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Survey $survey, UpdateSurveyRequest $request): Survey
+    {
+        $data = $request->validated();
+
+        if (isset($data['image'])) {
+            $data['image'] = $this->saveImage($data['image']);
+            if ($survey->image) {
+                File::delete(public_path($survey->image));
+            }
+        }
+
+        $survey->update($data);
+
+        $existingIds = $survey->questions()->pluck('id')->toArray();
+        $newIds = Arr::pluck($data['questions'], 'id');
+
+        $toDelete = array_diff($existingIds, $newIds);
+        $toAdd = array_diff($newIds, $existingIds);
+
+        SurveyQuestion::destroy($toDelete);
+
+        foreach ($data['questions'] as $question) {
+            if (in_array($question['id'], $toAdd)) {
+                $question['survey_id'] = $survey->id;
+                $this->createQuestion($question);
+            }
+        }
+
+        $questionMap = collect($data['questions'])->keyBy('id');
+        foreach ($survey->questions as $question) {
+            if (isset($questionMap[$question->id])) {
+                $this->updateQuestion($question, $questionMap[$question->id]);
+            }
+        }
+
+        return $survey;
+    }
+
+    /**
+     * @param Survey $survey
+     * @return Response
+     */
+    public function destroy(Survey $survey): Response
+    {
+        $survey->delete();
+
+        if ($survey->image) {
+            File::delete(public_path($survey->image));
+        }
+
+        return response('', 204);
+    }
+
+    /**
+     * @param $image
+     * @return string
+     * @throws \Exception
+     */
+    private function saveImage($image): string
+    {
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            $image = substr($image, strpos($image, ',') + 1);
+            $type = strtolower($type[1]);
+
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
+                throw new \Exception('Invalid image type.');
+            }
+
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false) {
+                throw new \Exception('Failed to base64 decode the image.');
+            }
+        } else {
+            throw new \Exception('Invalid image format.');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+
+        if (!File::exists($absolutePath)) {
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
+    }
+
+    /**
+     * Create a new survey question.
+     *
+     * @param array $data
+     * @return SurveyQuestion
+     * @throws \Illuminate\Validation\ValidationException
+     * @throws \JsonException
+     */
+    private function createQuestion(array $data): SurveyQuestion
+    {
+        $data = $this->prepareQuestionData($data);
+
+        $validator = Validator::make($data, $this->getQuestionValidationRules());
+
+        return SurveyQuestion::create($validator->validated());
+    }
+
+    /**
+     * Update an existing survey question.
+     *
+     * @param SurveyQuestion $question
+     * @param array $data
+     * @return bool
+     * @throws \Illuminate\Validation\ValidationException
+     * @throws \JsonException
+     */
+    private function updateQuestion(SurveyQuestion $question, array $data): bool
+    {
+        $data = $this->prepareQuestionData($data);
+
+        $validator = Validator::make($data, $this->getQuestionValidationRules());
+
+        return $question->update($validator->validated());
+    }
+
+    /**
+     * Prepare survey question data before validation.
+     *
+     * @param array $data
+     * @return array|string
+     * @throws \JsonException
+     */
+    private function prepareQuestionData(array $data): array|string
+    {
+        if (isset($data['data']) && is_array($data['data'])) {
+            $data['data'] = json_encode($data['data'], JSON_THROW_ON_ERROR);
+        }
+        return $data;
+    }
+
+    /**
+     * Get validation rules for survey questions.
+     *
+     * @return array
+     */
+    private function getQuestionValidationRules(): array
+    {
+        return [
+            'question' => 'required|string',
+            'type' => ['required', Rule::in([
+                Survey::TYPE_RADIO,
+                Survey::TYPE_CHECKBOX,
+                Survey::TYPE_SELECT,
+                Survey::TYPE_TEXT,
+                Survey::TYPE_TEXTAREA
+            ])],
+            'description' => 'nullable|string',
+            'data' => 'present',
+            'survey_id' => 'exists:App\Models\Survey,id'
+        ];
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): AnonymousResourceCollection
+    {
+        $user = $request->user();
+        return SurveyResource::collection(Survey::where('user_id', $user->id)->paginate(5));
+    }
+}
