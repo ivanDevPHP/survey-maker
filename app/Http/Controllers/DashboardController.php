c83b22124d144ad85d5f0064d2d70e8cbcc973b9
Ivan <ivanfslima@gmail.com>
2025-02-25 19:56:18 +0000

    refactor: move dashboard logic to DashboardService

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,43 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\SurveyAnswerResourse;
-use App\Http\Resources\SurveyResource;
-use App\Http\Resources\SurveyResourceDashboard;
-use App\Models\Survey;
-use App\Models\SurveyAnswer;
+use App\Services\DashboardService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request){
+    private DashboardService $dashboardService;
+
+    public function __construct(DashboardService $dashboardService)
+    {
+        $this->dashboardService = $dashboardService;
+    }
+
+    public function index(Request $request)
+    {
         $user = $request->user();
 
-        //Total Number of Surveys
-        $total = Survey::query()->where('user_id', $user->id)->count();
+        $data = $this->dashboardService->getDashboardData($user);
 
-        //Latest Survey
-        $latest = Survey::query()->where('user_id', $user->id)->latest('created_at')->first();
-
-        //Total Number of answers
-        $totalAnswers = SurveyAnswer::query()
-            ->join('surveys', 'survey_answers.survey_id', '=', 'surveys.id')
-            ->where('surveys.user_id', $user->id)
-            ->count();
-
-        //Latest 5 answers
-        $latestAnswers = SurveyAnswer::query()
-            ->join('surveys', 'survey_answers.survey_id', '=', 'surveys.id')
-            ->where('surveys.user_id', $user->id)
-            ->orderBy('end_date', 'DESC')
-            ->limit(5)
-            ->getModels('survey_answers.*');
-
-        return [
-            'totalSurveys' => $total,
-            'latestSurvey' => $latest ? new SurveyResourceDashboard($latest) : null,
-            'totalAnswers' => $totalAnswers,
-            'latestAnswers' => SurveyAnswerResourse::collection($latestAnswers)
-        ];
+        return response()->json($data);
     }
 }
